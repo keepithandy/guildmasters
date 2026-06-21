b72fbd2e32c0c40b4faa9a6679a88782cf8e4628
John Belles <qualify155@gmail.com>
2026-06-21 18:46:10 -0400

    Add startup wiring

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { resolveContracts, startContract } from './contracts.js';
+import { recruitHero } from './heroes.js';
+import { loadGame, resetGame, saveGame } from './saveSystem.js';
+import { render } from './ui.js';
+
+let state = loadGame();
+state = resolveContracts(state);
+saveGame(state);
+
+const actions = {
+  recruitHero() {
+    state = recruitHero(state);
+    saveAndRender();
+  },
+  startContract(heroId, contractId) {
+    state = startContract(state, heroId, contractId);
+    saveAndRender();
+  },
+  resetGame() {
+    if (!confirm('Reset Guildmasters progress?')) return;
+    state = resetGame();
+    saveAndRender();
+  }
+};
+
+function saveAndRender() {
+  state = resolveContracts(state);
+  saveGame(state);
+  render(state, actions);
+}
+
+setInterval(saveAndRender, 1000);
+render(state, actions);
